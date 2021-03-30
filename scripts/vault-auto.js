12f794f86f340777396e2255ad06d2ad9fefd8f0
LordForkuaad <vitaly.blogic@gmail.com>
2021-03-30 11:56:18 +0200

    Adding Vault-Auto Script

---
+++ scripts/vault-auto.js
@@ -0,0 +1,54 @@
+const Web3 = require("web3");
+const Strategy = require('../build/contracts/StrategyCafeLP.json');
+
+const fromAddress = "";
+const vaultAddresses = [""];
+const privateKey = "";
+const chainID = 97;
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://data-seed-prebsc-1-s1.binance.org:8545"
+  )
+);
+
+let strategyContract;
+
+const harvest = async (vaultAddress) => {
+  strategyContract = new web3.eth.Contract(Strategy.abi, vaultAddress);
+
+  const nonce = await web3.eth.getTransactionCount(fromAddress);
+  const gasPriceWei = await web3.eth.getGasPrice();
+  const data = strategyContract.methods.harvest().encodeABI()
+
+  const signedTx  = await web3.eth.accounts.signTransaction({
+      to: vaultAddress,
+      gas: 2000000,
+      data: data,
+      gasPrice: gasPriceWei,
+      nonce: nonce,
+      chainId: chainID
+  }, privateKey)
+
+  await web3.eth.sendSignedTransaction(signedTx.rawTransaction || signedTx.rawTransaction);
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+async function main() {
+  
+  while(1) {
+    try {
+        for (var i = 0; i < vaultAddresses.length; i++) {
+            await harvest(vaultAddresses[i]);
+        }
+    }catch(err) {
+        console.log(err);
+    }
+    await sleep(60000);
+  }
+}
+
+main();
